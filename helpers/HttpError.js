0d0ba5508edb6f06e95ff942fbe703f461e3684f
Vova-Kalbenko <lastforhs3@gmail.com>
2023-10-23 10:38:57 +0300

    ready-1

--- helpers/HttpError.js
+++ helpers/HttpError.js
@@ -1,7 +1,15 @@
-const HttpError = (status, message) => {
+const errorMessage = {
+    400: "Bad request",
+    401: "Not authorized",
+    403: "Forbidden",
+    404: "Not found",
+    409: "Conflict",
+  };
+  
+  const HttpError = (status, message = errorMessage[status]) => {
     const error = new Error(message);
     error.status = status;
     return error;
-}
-
-module.exports = HttpError;
+  };
+  
+  module.exports = HttpError;
